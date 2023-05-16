2022b910b8611e97e2860aeb69291ff0c370a6a2
greatchriz <89561544+greatchriz@users.noreply.github.com>
2023-05-16 21:07:12 +0100

    home page changed

---
+++ resources/views/frontend/home.bladeee.php
@@ -0,0 +1,92 @@
+<x-frontend.layout>
+    <x-frontend.sections.home.hero.layout>
+
+
+        <x-frontend.sections.home.hero.slide
+        slidernumber="1"
+        bgImage="fundpro/images/bg/bg24.jpg"
+       />
+
+       <x-frontend.sections.home.hero.slide
+        slidernumber="2"
+        bgImage="fundpro/images/bg/bg24.jpg"
+       />
+
+         <x-frontend.sections.home.hero.slide
+          slidernumber="3"
+          bgImage="fundpro/images/bg/bg24.jpg"
+        />
+
+    </x-frontend.sections.home.hero.layout>
+
+
+    <x-frontend.sections.home.boxes.layout>
+        <x-frontend.sections.home.boxes.box />
+        <x-frontend.sections.home.boxes.box />
+        <x-frontend.sections.home.boxes.box />
+    </x-frontend.sections.home.boxes.layout>
+
+    <x-frontend.sections.home.about.layout />
+
+    <x-frontend.sections.home.causes.layout>
+        <x-frontend.sections.home.causes.cause />
+    </x-frontend.sections.home.causes.layout>
+
+    <x-frontend.sections.home.funfact.layout>
+        <x-frontend.sections.home.funfact.funfact />
+    </x-frontend.sections.home.funfact.layout>
+
+    <x-frontend.sections.home.mission-and-events.layout>
+        <x-frontend.sections.home.mission-and-events.mission />
+        {{-- add this to the names slot events --}}
+        <x-slot:events>
+            <x-frontend.sections.home.mission-and-events.event />
+        </x-slot>
+    </x-frontend.sections.home.mission-and-events.layout>
+
+    <x-frontend.sections.home.volunteers.layout>
+        <x-frontend.sections.home.volunteers.volunteer />
+
+    </x-frontend.sections.home.volunteers.layout>
+
+    <x-frontend.sections.home.emergency.layout />
+
+    <x-frontend.sections.home.be-a-volunteer.layout>
+        <x-frontend.sections.home.be-a-volunteer.form />
+    </x-frontend.sections.home.be-a-volunteer.layout>
+
+    <x-frontend.sections.home.emergency.layout>
+
+    </x-frontend.sections.home.emergency.layout>
+
+
+    <x-frontend.sections.home.testimonials.layout>
+        <x-frontend.sections.home.testimonials.testimonial />
+
+    </x-frontend.sections.home.emergency.layout>
+
+
+
+
+    <x-frontend.sections.home.gallery.layout>
+        <x-frontend.sections.home.gallery.item />
+
+    </x-frontend.sections.home.gallery.layout>
+
+
+
+
+
+
+
+
+
+
+
+
+    {{-- //home/cause/layout --}}
+
+
+
+
+</x-frontend.layout>
