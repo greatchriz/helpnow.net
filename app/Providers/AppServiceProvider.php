b58a48b73957fdd1d5e1a8524be2db65d823432f
greatchriz <89561544+greatchriz@users.noreply.github.com>
2023-07-19 01:20:41 +0100

    Update AppServiceProvider.php

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -3,6 +3,8 @@
 namespace App\Providers;
 
 use Illuminate\Support\ServiceProvider;
+use Illuminate\Support\Facades\Schema;
+
 
 class AppServiceProvider extends ServiceProvider
 {
@@ -19,6 +21,9 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot(): void
     {
+
+        Schema::defaultStringLength(191);
+
         //make pages available to all views
         view()->composer('*', function ($view) {
             $view->with('pages', \App\Models\Page::all());
